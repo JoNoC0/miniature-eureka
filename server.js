4d0ad4231b1c93bf349b33639ccb222e2b1c4cce
JoNoC0 <jnguyeningco@gmail.com>
2021-12-05 23:56:20 -0600

    added app.delete

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const { notes1 } = require('./db/db.json');
-const { default: ShortUniqueId } = require('short-unique-id');
-const idNumber = new ShortUniqueId();
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,13 +9,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
-
-function findById(id, notesArray) {
-    console.log(notesArray);
-    const result = notesArray.filter(item => item.id === id)[0];
-    
-    return result;
-};
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
@@ -40,7 +31,7 @@ app.post('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', function(err, data) {
         if (err) throw err
         let notes = JSON.parse(data)
-        req.body.id = idNumber();
+        // req.body.id = idNumber();
         console.log(notes);
         console.log(req.body);
         notes.push(req.body);
@@ -53,34 +44,22 @@ app.post('/api/notes', (req, res) => {
     })
 });
 
-// req.params.id
-    // research filter method; looping setup not
-    // need to start with fs.readfile
 
 app.delete('/api/notes/:id', function(req, res) {
-    console.log(notes1);
-    let result = findById(req.params.id, notes1)
-    console.log(result);
-
-    // fs.readFile('./db/db.json', function(err, data) {
-    //     if (err) throw err
-    //     console.log(note);
-    //     let notes = JSON.parse(data)
-        
-    //     
-    //     console.log(deleteNote);
-    //     // for (let i = 0; i < notes.length; i++) {
-    //     //     if (notes[i].id === Number(deleteNote)) {
-    //     //       notes.splice([i], 1);
-    //     //     }
-    //     // }
-    //     let data2 = JSON.stringify(notes)
-    //     fs.writeFile("./db/db.json", data2, function(err, data) {
-    //         if (err) throw err
-    //     res.json(notes);
-    //     })
-    // });
-})
+    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+    res.json(savedNotes);
+});
 
 
 
